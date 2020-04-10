f0d03746d069654d3c117a99680b058996053132
Cydie <cydie00@gmail.com>
2020-04-10 20:05:07 +0200

    GT4500  Torpedo FiringMode.ALL fix

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,12 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        if (! primaryTorpedoStore.isEmpty()) {
+          firingSuccess = primaryTorpedoStore.fire(1);
+        }
+        if (! secondaryTorpedoStore.isEmpty()) {
+          firingSuccess = secondaryTorpedoStore.fire(1) && firingSuccess;
+        }
 
         break;
     }
